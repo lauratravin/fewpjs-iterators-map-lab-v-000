55ca0d9eb8a2c373e466d51018734ddcff87d381
Laura Messa Travin <laura.travin@gmail.com>
2021-05-04 20:21:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.maps( function(tutorial){
+         tutorial =  tutorial.split(' ')
+         for(let t = 0; t < tutorial.length; t++ ){
+           tutorial[t].charAt(0).toUpperCase()+tutorial[t].slice(1)
+         }
+         return tutorial.join(' ')
+  } )
 }
